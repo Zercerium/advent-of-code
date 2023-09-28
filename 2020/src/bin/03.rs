4fba1f51563fb127c942e8d02e2959f448b1a4e1
Nick Kotsakidis <zercerium@mailbox.org>
2023-09-28 10:47:58 +0200

    add 2020 03

---
+++ 2020/src/bin/03.rs
@@ -0,0 +1,56 @@
+use aoc_util::map::{Map2D, Point2D};
+
+// Day 3: Toboggan Trajectory
+fn main() {
+    let file = aoc_util::read_input_file(2020, 3);
+    let map = parse(&file);
+    // Part 1
+    let slope = (3, 1);
+    let trees = count_trees(&map, slope);
+    println!("Result Part 1: {:?}", trees);
+
+    // Part 2
+    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let trees = slopes
+        .iter()
+        .map(|s| count_trees(&map, *s))
+        .fold(1, |acc, t| acc * t);
+    println!("Result Part 2: {:?}", trees);
+}
+
+fn count_trees(map: &Map2D<MapItem>, slope: (u16, u16)) -> usize {
+    let mut location = Point2D::<u16>::new(0, 0);
+    let mut trees = 0;
+
+    location.add(slope);
+    while location.y < map.columns() as u16 {
+        if *map.get(location) == MapItem::Tree {
+            trees += 1;
+        }
+        location.add(slope);
+    }
+    trees
+}
+
+fn parse(s: &str) -> Map2D<MapItem> {
+    let length = s.lines().next().unwrap().len();
+    let mut map = Map2D::new(length);
+    let f = |s: &str| -> Vec<MapItem> {
+        s.chars()
+            .map(|c| match c {
+                '.' => MapItem::Nothing,
+                '#' => MapItem::Tree,
+                _ => unreachable!(),
+            })
+            .collect()
+    };
+    s.lines().for_each(|s| map.append_row(&mut f(s)));
+    map.set_x_torus();
+    map
+}
+
+#[derive(Clone, PartialEq, Eq)]
+enum MapItem {
+    Tree,
+    Nothing,
+}
--- aoc_util/src/map.rs
+++ aoc_util/src/map.rs
@@ -1,29 +1,144 @@
+#[derive(Clone)]
 pub struct Map2D<T> {
     map: Vec<T>,
     row_length: usize,
     start_index: usize,
+    x_torus: bool,
+    y_torus: bool,
 }
 
-impl<T> Map2D<T> {
+// pls be aware this is just for AoC and not for production use,
+// so there are no checks for out of bonds requests, cause i don't want to return Results or Options
+// happy panicking
+impl<T> Map2D<T>
+where
+    T: Clone,
+{
+    pub fn new_default(x_size: usize, y_size: usize, default: T, start_index: usize) -> Self {
+        let size = x_size * y_size;
+        let mut map = Vec::with_capacity(size);
+        (0..size).for_each(|_| map.push(default.clone()));
+        Map2D {
+            map,
+            row_length: x_size,
+            start_index,
+            x_torus: false,
+            y_torus: false,
+        }
+    }
+
+    pub fn new(row_length: usize) -> Self {
+        Map2D {
+            map: vec![],
+            row_length,
+            start_index: 0,
+            x_torus: false,
+            y_torus: false,
+        }
+    }
+
+    pub fn append_row(&mut self, mut items: &mut Vec<T>) {
+        assert_eq!(self.row_length, items.len());
+        self.map.append(&mut items);
+    }
+
+    pub fn set_x_torus(&mut self) {
+        self.x_torus = true;
+    }
+
+    pub fn set_y_torus(&mut self) {
+        self.y_torus = true;
+    }
+
+    fn index<U>(&self, p: Point2D<U>) -> usize
+    where
+        U: Into<usize> + Copy,
+    {
+        let mut x: usize = p.x.into() - self.start_index;
+        if self.x_torus {
+            x = x % self.row_length
+        };
+        assert!(x < self.row_length);
+        x + (p.y.into() - self.start_index) * self.row_length
+    }
+
     pub fn get<U>(&self, p: Point2D<U>) -> &T
     where
-        U: Into<usize>,
+        U: Into<usize> + Copy,
     {
-        let index =
-            p.x.into() - self.start_index + (p.y.into() - self.start_index) * self.row_length;
+        let index = self.index(p);
         &self.map[index]
+    }
+
+    pub fn set<U>(&mut self, p: Point2D<U>, element: T)
+    where
+        U: Into<usize> + Copy,
+    {
+        let index = self.index(p);
+        self.map[index] = element;
+    }
+
+    pub fn columns(&self) -> usize {
+        self.map.len() / self.row_length
     }
 }
 
-pub struct Point2D<T> {
+#[derive(Clone, Copy)]
+pub struct Point2D<T>
+where
+    T: Copy,
+{
     // column
     pub x: T,
     // row
     pub y: T,
 }
 
-impl<T> Point2D<T> {
+impl<T> Point2D<T>
+where
+    T: std::ops::Add<Output = T> + Copy,
+{
     pub fn new(x: T, y: T) -> Self {
         Self { x, y }
+    }
+
+    pub fn add_x(&mut self, x: T) {
+        self.x = self.x + x;
+    }
+
+    pub fn add_y(&mut self, y: T) {
+        self.y = self.y + y;
+    }
+
+    pub fn add(&mut self, a: (T, T)) {
+        self.x = self.x + a.0;
+        self.y = self.y + a.1;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_get() {
+        let mut map = Map2D::new_default(4, 3, 0, 1);
+        map.set(Point2D::<u8>::new(1, 1), 1);
+        map.set(Point2D::<u8>::new(4, 3), 9);
+        map.set(Point2D::<u8>::new(2, 2), 7);
+        map.set(Point2D::<u8>::new(4, 1), 6);
+
+        #[rustfmt::skip]
+        let vec = vec![
+        1,0,0,6,
+        0,7,0,0,
+        0,0,0,9];
+
+        assert_eq!(map.map, vec);
+
+        map.set_x_torus();
+        assert_eq!(*map.get(Point2D::<u8>::new(8, 1)), 6);
+
+        assert_eq!(map.columns(), 3);
     }
 }
